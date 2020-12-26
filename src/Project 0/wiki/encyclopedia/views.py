ed553db41f0165673686de4db9393ba2d4c72cc4
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-26 16:06:47 +1100

    Start to parse markdown to HTML

--- src/Project 0/wiki/encyclopedia/views.py
+++ src/Project 0/wiki/encyclopedia/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import os
+import markdown2
 from . import util
 
 def index(request):
@@ -12,6 +14,14 @@ def content(request, word):
     md_word = word + '.md'
     if (md_word in entries_lst):
         print("css.md is in here")
+    
+    content_html = """<div> <h1>heading</h1> </div>
+    <p>CSS is cook</p>
+    """
+    with open('encyclopedia/templates/encyclopedia/filter.html', 'w') as f:
+        #src\Project 0\wiki\encyclopedia\templates\encyclopedia\filter.html
+        f.write(content_html)
+    
     return render(request, 'encyclopedia/content.html', {
         'word': word
     })
