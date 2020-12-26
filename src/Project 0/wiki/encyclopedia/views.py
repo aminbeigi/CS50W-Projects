259672f57aca38507a8285ef1e5634bf26c34712
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-26 15:26:10 +1100

    Add a route

--- src/Project 0/wiki/encyclopedia/views.py
+++ src/Project 0/wiki/encyclopedia/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-
+import os
 from . import util
 
-
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+    return render(request, 'encyclopedia/index.html', {
+        'entries': util.list_entries()
     })
 
+def content(request, word):
+    entries_lst = os.listdir('entries')
+    md_word = word + '.md'
+    if (md_word in entries_lst):
+        print("css.md is in here")
+    return render(request, 'encyclopedia/content.html', {
+        'word': word
+    })
