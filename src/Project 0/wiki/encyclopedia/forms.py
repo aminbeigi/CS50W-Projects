28d9ccbb98ae9993ed52bab26f7c2fb213009a83
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-29 19:38:53 +1100

    Add prepopulated text

--- src/Project 0/wiki/encyclopedia/forms.py
+++ src/Project 0/wiki/encyclopedia/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class EntryForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=25)
-    content = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(initial='Title', label='Title', max_length=25)
+    content = forms.CharField(initial= ("## Sample Markdown Heading\n"
+                                        "**Lorem Ipsum** is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."), 
+                                        widget=forms.Textarea)
