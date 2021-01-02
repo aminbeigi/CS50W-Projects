c18c163bd2ae67f47257045ee2eed28cb4ef85a1
Amin Ghasembeigi <me@aminbeigi.com>
2021-01-03 09:37:08 +1100

    Add User to admin page

--- src/Project 2/commerce/auctions/models.py
+++ src/Project 2/commerce/auctions/models.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
+"""
+test_user
+cY4jmdE0
+"""
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
