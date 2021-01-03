669511650d11aa83706299c1faffcaa8f6a89048
Amin Ghasembeigi <me@aminbeigi.com>
2021-01-03 14:29:04 +1100

    Add users app

---
+++ src/Project 2/commerce/users/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render
+from django.db import IntegrityError
+
+from auctions.models import User, Listing
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return redirect("index")
+    else:
+        return render(request, "auctions/register.html")
