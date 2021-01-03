ad1c7475cafa9c31f6fb63fb9ec216d0950a4260
Amin Ghasembeigi <me@aminbeigi.com>
2021-01-03 14:04:12 +1100

    Repalce httpd stuff with redirect shortcut

--- src/Project 2/commerce/commerce/settings.py
+++ src/Project 2/commerce/commerce/settings.py
@@ -32,8 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'auctions',
-    'users',
-    'cripsy_forms',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
