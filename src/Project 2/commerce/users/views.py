46213abeb2635ef90b561ae276ed1be4d42f0080
Amin Ghasembeigi <me@aminbeigi.com>
2021-01-03 14:43:37 +1100

    Add new register

--- src/Project 2/commerce/users/views.py
+++ src/Project 2/commerce/users/views.py
@@ -1,19 +1,36 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.db import IntegrityError
+from django.contrib import messages
 
 from auctions.models import User, Listing
 
 def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})    
+    
+"""
+def register(request):
+    form = UserCreationForm()
     if request.method == "POST":
+        form = UserCreationForm(request.POST)
         username = request.POST["username"]
         email = request.POST["email"]
-
+        
         # Ensure password matches confirmation
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "auctions/register.html", {
+            return render(request, "users/register.html", {
                 "message": "Passwords must match."
             })
 
@@ -22,10 +39,11 @@ def register(request):
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
-            return render(request, "auctions/register.html", {
+            return render(request, "users/register.html", {
                 "message": "Username already taken."
             })
         login(request, user)
         return redirect("index")
     else:
-        return render(request, "auctions/register.html")
+        return render(request, "users/register.html")
+"""
