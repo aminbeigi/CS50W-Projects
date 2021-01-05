b64a1a250378d2a4f3b4c1b049b1121b163b5a11
Amin Ghasembeigi <me@aminbeigi.com>
2021-01-05 16:50:34 +1100

    Start to add bid model

--- src/Project 2/commerce/auctions/models.py
+++ src/Project 2/commerce/auctions/models.py
@@ -9,9 +9,19 @@ class Listing(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
     image = models.ImageField(default='default.jpg', upload_to='images', blank=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return f'{self.title}'
+
+class Bid(models.Model):
+    bid = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"the bid of {self.listing}"
