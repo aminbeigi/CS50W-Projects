d640c54901484ad8baf1bc97239eb61e927c4080
Amin Ghasembeigi <me@aminbeigi.com>
2021-01-05 12:00:06 +1100

    Add images

--- src/Project 2/commerce/auctions/models.py
+++ src/Project 2/commerce/auctions/models.py
@@ -2,11 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
-"""
-test_user
-cY4jmdE0
-"""
-
 class User(AbstractUser):
     pass
 
@@ -15,7 +10,7 @@ class Listing(models.Model):
     description = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    #image = models.ImageField(upload_to='static/auctions/images')
+    image = models.ImageField(default='default.jpg', upload_to='images')
 
     def __str__(self):
         return f'{self.title}'
