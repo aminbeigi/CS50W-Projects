3bb6d8ee700e84f32ec0f9dcef8dba3ce46b92cd
Amin Ghasembeigi <me@aminbeigi.com>
2021-01-13 23:11:19 +1100

    Add min value for price

--- src/Project 2/commerce/auctions/models.py
+++ src/Project 2/commerce/auctions/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 '''
 John
@@ -15,7 +16,7 @@ class Listing(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, validators=[MinValueValidator(Decimal('0.01'))])
     image = models.ImageField(default='default.jpg', upload_to='images', blank=True)
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -34,7 +35,7 @@ class Comment(models.Model):
         return f"comment by {self.author.username} in {self.listing.title}"
 
 class Bid(models.Model):
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, validators=[MinValueValidator(Decimal('0.01'))])
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
