e7b620a48ffbd876b63adafdd18c6682f5c5c274
Amin Ghasembeigi <me@aminbeigi.com>
2021-01-29 11:45:25 +1100

    Add Post templates

--- src/Project 4/app/src/index.js
+++ src/Project 4/app/src/index.js
@@ -4,21 +4,60 @@ import './index.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Button, Breadcrumb } from 'react-bootstrap'
+import { Breadcrumb, Card, Container } from 'react-bootstrap'
+
+const CreateNewPost = () => {
+    return (
+        <section>
+            <Card>
+            <Card.Header as="h5">Create new post</Card.Header>
+                <Card.Body>
+                    <Card.Title>title: <input></input></Card.Title>
+                    <Card.Text>Body: <input></input></Card.Text>
+                </Card.Body>
+            </Card>
+        </section>
+    )
+}
+const Post = ({title, body}) => {
+    return (
+        <section>
+            <Card>
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>{body}</Card.Text>
+                </Card.Body>
+            </Card>
+        </section>
+    )
+}
+
+const NavBar = () => {
+    return (
+        <div>
+            <Breadcrumb>
+                <Breadcrumb.Item active>Home</Breadcrumb.Item>
+                <Breadcrumb.Item href="#">All Posts</Breadcrumb.Item>
+            </Breadcrumb>
+        </div>
+    )
+}
 
 class Index extends Component {
     render() {
         return (
             <div>
-                <div>
-                    <Breadcrumb>
-                        <Breadcrumb.Item active>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item href="#">All Posts</Breadcrumb.Item>
-                    </Breadcrumb>
-                </div>
-                <div>
+                <NavBar />
+
+                <Container>
+                    <br></br>
+                    <CreateNewPost />
+                    <br></br>
                     <h1>All Posts</h1>
-                </div>
+                    <Post title='titlkkkkkkkkkkkke' body='body'/>
+                    <Post title='title2' body='body2'/>
+                    <Post title='title3' body='body3'/>
+                </Container>
             </div>
         )
     }
