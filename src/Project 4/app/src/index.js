577d4fbf7bfe9bd8cd86bc01eec209381946b77f
Amin Ghasembeigi <me@aminbeigi.com>
2021-01-31 12:46:44 +1100

    Add stuff back

--- src/Project 4/app/src/index.js
+++ src/Project 4/app/src/index.js
@@ -6,37 +6,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Button, Breadcrumb, Card, Container } from 'react-bootstrap';
 
+import * as serviceWorker from './serviceWorker';
+
 const API_URL = 'http://localhost:8000';
 const PAGE_POST_LIMIT = 5;
 
-const postData = e => {
-    const title = e.target[0].value;
-    const body = e.target[1].value;
-    const data = {
-        'user': 'George',
-        'title': title,
-        'body': body,
+const CreateNewPost = ({onCreatePost}) => {
+    const postData = e => {
+        const title = e.target[0].value;
+        const body = e.target[1].value;
+        const data = {
+            'user': 'George',
+            'title': title,
+            'body': body,
+        }
+        console.log(data);
+
+        fetch(API_URL + '/create-post', {
+            method: 'POST',
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            if (onCreatePost) {
+                onCreatePost()
+            }
+
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });    
+
+        e.target[0].value = '';
+        e.target[1].value = '';
+        e.preventDefault();
     }
-    console.log(data);
-    
-    fetch(API_URL + '/create-post', {
-        method: 'POST',
-        body: JSON.stringify(data),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });    
 
-    e.target[0].value = '';
-    e.target[1].value = '';
-    e.preventDefault();
-}
-
-const CreateNewPost = () => {
     return (
         <section>
             <Card>
@@ -52,6 +58,7 @@ const CreateNewPost = () => {
         </section>
     )
 }
+
 const Post = ({title, body, user, timestamp}) => {
     return (
         <section>
@@ -85,13 +92,17 @@ const Index = () => {
         }
     )
 
-    useEffect(() => {
-        console.log("The component is now mounted!")
-        setState({loading: true})
-        fetch(API_URL + '/posts')
+    const fetchData = () => {
+        //setState({loading: true})
+         fetch(API_URL + '/posts')
             .then(response => response.json())
             .then(response => setState({data: response, loading: false}))
-            .catch(() => console.log(`Can't access ${API_URL + '/posts'} response.`))
+            .catch(() => console.log(`Can't access ${API_URL + '/posts'} response.`))       
+    }
+
+    useEffect(() => {
+        console.log("The component is now mounted!");
+        fetchData();
     }, [])
 
         return (
@@ -99,16 +110,16 @@ const Index = () => {
                 <NavBar />
                 <Container>
                     <br></br>
-                    <CreateNewPost />
+                    <CreateNewPost onCreatePost={fetchData}/>
                     <br></br>
                     <h1>All Posts</h1>
                     {state.loading
                         ? "loading..."
                         : <div>
-                            {state.data.map((post, i) => {
+                            {state.data.map((post) => {
                                 return(
-                                    <div key={i}>
-                                        <Post key={i} title={post['title']} body={post['body']} user={post['user']} timestamp={post['timestamp']} />
+                                    <div key={post.id}>
+                                        <Post title={post['title']} body={post['body']} user={post['user']} timestamp={post['timestamp']} />
                                     </div>
                                 )
                             })}
@@ -124,3 +135,8 @@ ReactDOM.render(
     <Index />,
     document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
